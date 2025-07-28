1780d72f46d247c5b067b72e20c113cc3bf52db7
JenikP <95909702+JenikP@users.noreply.github.com>
2025-07-28 12:37:01 +1000

    Update img tag in the navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar = () => {
               <img 
   src="/lovable-uploads/c3344f81-1e98-4f81-9573-1d658bc58c98.png" 
   alt="Brow Arc Threading Logo" 
-  className="h-20 md:h-24 lg:h-28 xl:h-32 w-auto max-h-[80px] sm:max-h-[96px] md:max-h-[112px] lg:max-h-[128px]"
+  className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] h-auto max-h-[64px] sm:max-h-[80px] md:max-h-[96px] lg:max-h-[112px]"
 />
 
             </Link>
