e45e6d2fb5ce12311a0c7e1742ecc17773b2f0f9
JenikP <95909702+JenikP@users.noreply.github.com>
2026-06-05 13:08:22 +1000

    Update ServiceCategory.tsx

--- src/components/services/ServiceCategory.tsx
+++ src/components/services/ServiceCategory.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import ServiceCard from './ServiceCard';
-import fullBackImg from '../../assets/fullback.jpg';
-import sideburnImg from '../../assets/sideburn.jpg.';
+import sideburnImg from '../../assets/sideburns.jpg';
+import fullBackImg from '../../assets/full-back.jpg';
 
 interface Service {
   title: string;
